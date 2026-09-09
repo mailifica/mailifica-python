e606790f7d8ff9bfac22b1d8cfb4e434c2d2c21d
tarrciodev <tarcisio.devmaster@gmail.com>
2026-09-09 11:01:07 +0100

    chore: bump version to 1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mailifica",
-    version="1.0.1",
+    version="1.0.2",
     description="Official Python SDK for Mailifica Email Infrastructure",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
